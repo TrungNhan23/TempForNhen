1d35ab9094e277923d4bc1f41e67a0176dbd4d00
hongkiet456@gmai.com <hongkiet456@gmai.com>
2024-09-08 20:23:07 +0700

    update vector code

--- VectorReimplement/inc/vec.h
+++ VectorReimplement/inc/vec.h
@@ -4,10 +4,10 @@
 #include <iostream>
 
 template <typename type>
-class vector
+class vec
 {
 private:
-    // this arr which stores the address of vector
+    // this arr which stores the address of vec
     type *arr;
     // total storage
     int cap;
@@ -15,14 +15,86 @@ private:
     int current;
 
 public:
-    vector();
-    ~vector();
+    vec();
+    ~vec();
+    // vec &operator=(const vec &vec);
+    // vec &operator=(const vec &&vec);
+    // vec &operator=(initializer_list <type> il);
     void push_back(type value);
     void pop_back();
     int size();
     int capacity();
     void insert(type value, int pos);
-
+    bool isEmpty(); 
     void show();
 };
+
+#include "vec.h"
+
+template <typename type>
+vec<type>::vec()
+{
+    arr = new type[1];
+    cap = 1;
+    current = 0;
+}
+
+template <typename type>
+vec<type>::~vec()
+{
+    delete[] arr;
+}
+
+template <typename type>
+int vec<type>::size()
+{
+    return current;
+}
+
+template <typename type>
+int vec<type>::capacity()
+{
+    return cap;
+}
+
+template <typename type>
+void vec<type>::show()
+{
+    for (int i = 0; i < current; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+template <typename type>
+void vec<type>::push_back(type value)
+{
+    if (current == cap)
+    {
+        type *temp = new type[2 * cap];
+
+        for (int i = 0; i < cap; i++)
+        {
+            temp[i] = arr[i];
+        }
+
+        delete[] arr;
+        cap *= 2;
+        arr = temp;
+    }
+
+    arr[current] = value;
+    current++;
+}
+
+template <typename type> 
+void vec<type>::pop_back(){
+    if(current == 0) return; 
+    current--;
+}
+
+template <typename type> 
+bool vec<type>::isEmpty(){
+    return (current == 0) ? true:false;
+}
+
 #endif
--- VectorReimplement/src/main.cpp
+++ VectorReimplement/src/main.cpp
@@ -1,10 +1,11 @@
 #include "vec.h"
-
+#include <iostream>
 using namespace std; 
 
 
 int main(){
-    vector<int> vec;
+    int a; 
+    vec<int> vec;
 
     vec.push_back(2);
     
@@ -12,7 +13,11 @@ int main(){
     
     vec.push_back(6);
     
-    vec.push_back(21);
-
+    vec.push_back(100);
+    
     vec.show();  
+    vec.pop_back(); 
+    vec.show(); 
+    std::cout << "my vector size: " << vec.isEmpty() << std::endl; 
+    return 0;
 }
